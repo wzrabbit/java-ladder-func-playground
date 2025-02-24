ed36c10a1c44e5ba13634abe2de94c89abf4664c
wzrabbit <curious.wzrabbit@gmail.com>
2025-02-25 01:52:32 +0900

    ✨ feat: 사다리 클래스를 구현
    - 게임의 정보는 담지 않은 채로 사다리 본연의 로직에만 집중하는 클래스
    - 사다리의 시작점과 도착점이 각각 어떻게 연결되는지를 인덱스 형태로 반환 가능

---
+++ src/main/java/model/Ladder.java
@@ -0,0 +1,58 @@
+package model;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+
+public class Ladder {
+    private final List<LadderRow> ladder;
+    private final int rowCount;
+    private final int columnCount;
+
+    public Ladder(List<LadderRow> ladder) {
+        validateLadder(ladder);
+        this.ladder = new ArrayList<>(ladder);
+        this.rowCount = ladder.size();
+        this.columnCount = ladder.get(0).size() + 1;
+    }
+
+    private void validateLadder(List<LadderRow> ladder) {
+        boolean isAllLadderRowSizeSame = ladder.stream()
+                .map(LadderRow::size)
+                .distinct()
+                .count() == 1;
+
+        if (!isAllLadderRowSizeSame) {
+            throw new IllegalArgumentException("사다리를 이루는 행의 크기는 모두 같아야 합니다.");
+        }
+    }
+
+    private int getBottomIndex(int topIndex) {
+        int column = topIndex;
+
+        for (int row = 0; row < rowCount; row += 1) {
+            if (column > 0 && ladder.get(row).get(column - 1)) {
+                column -= 1;
+                continue;
+            }
+
+            if (column < columnCount - 1 && ladder.get(row).get(column)) {
+                column += 1;
+            }
+        }
+
+        return column;
+    }
+
+    public List<Integer> getConnections() {
+        return IntStream.range(0, columnCount)
+                .mapToObj((topIndex) -> getBottomIndex(topIndex))
+                .collect(Collectors.toList());
+    }
+
+    public List<LadderRow> getLadder() {
+        return Collections.unmodifiableList(ladder);
+    }
+}
